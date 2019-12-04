d09edda29e22766fb7bd921ab958a453d52e8911
Vladislav Elizarov <galogen13@gmail.com>
2019-12-05 02:51:52 +0300

    HW1 v1

---
+++ hw1/go.mod
@@ -0,0 +1,8 @@
+module github.com/galogen13/otus/hw1
+
+go 1.12
+
+require (
+	github.com/beevik/ntp v0.2.0
+	golang.org/x/net v0.0.0-20191204025024-5ee1b9f4859a // indirect
+)
---
+++ hw1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	//time, err := ntp.Time("time.nist.gov")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	const layout = "02.01.2006 15:04:05 (MST)"
+	fmt.Printf("Current Local Time: %s", time.Local().Format(layout))
+}
