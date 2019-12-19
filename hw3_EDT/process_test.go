ac3172f1a3256a0fc564488ca84ed020323308ee
Vladislav Elizarov <galogen13@gmail.com>
2019-12-19 23:08:18 +0300

    переименована основаная функция в тестах

--- hw3_EDT/process_test.go
+++ hw3_EDT/process_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestProcess(t *testing.T) {
+func TestUnpack(t *testing.T) {
 
 	type result struct {
 		str string
@@ -18,7 +18,7 @@ func TestProcess(t *testing.T) {
 		"qwe\\4\\5":               {"qwe45", 0},
 		"a4bc2d5eqwe\\\\5j10\\l4": {"aaaabccdddddeqwe\\\\\\\\\\jjjjjjjjjjllll", 0}}
 	for key, val := range testSeq {
-		if str, err := Process(key); str != val.str || err != val.err {
+		if str, err := Unpack(key); str != val.str || err != val.err {
 			t.Fatalf("EXPECTED STR %s RESULT STR %s; EXPECTED ERR %d RESULT ERR %d", val.str, str, val.err, err)
 		}
 	}
