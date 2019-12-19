26b9cd56f94e01308f9702037ede1d006bc93bab
Vladislav Elizarov <galogen13@gmail.com>
2019-12-19 05:02:49 +0300

    Объединение функций в одну по рекомендации преподавателя

--- hw3_EDT/main.go
+++ hw3_EDT/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"unicode/utf8"
+	"unicode"
 )
 
 func main() {
@@ -13,99 +13,71 @@ func main() {
 	res := ""
 	err := 0
 
-	start = "a4bc2d5e"
-	res, err = Process1(start)
+	start = "a4bc2d5eg\\f3"
+	res, err = Process(start)
 	fmt.Println(start, res, err)
 
-	start = "abcd"
-	res, err = Process1(start)
-	fmt.Println(start, res, err)
-
-	start = "a22"
-	res, err = Process1(start)
-	fmt.Println(start, res, err)
-
-	start = "45"
-	res, err = Process1(start)
-	fmt.Println(start, res, err)
-
-	start = "qwe\\45"
-	res = Process2(start)
-	fmt.Println(start, res)
-
-	start = "qwe\\4\\5"
-	res = Process2(start)
-	fmt.Println(start, res)
-
-	start = "qwe\\\\5"
-	res = Process2(start)
-	fmt.Println(start, res)
 }
 
-func Process1(str string) (res string, err int) {
-	var r rune
-	var countBuilder strings.Builder
+// Process  #
+func Process(str string) (string, int) {
+	const escapeSymbol = '\\'
+	runes := []rune(str)
+	strLen := len(runes)
 	var resBuilder strings.Builder
-
-	for _, val := range str {
+	for i := 0; i < strLen; {
 		switch {
-		case r == 0 && isLetter(val):
-			r = val
-		case r != 0 && isNumeric(val):
-			countBuilder.WriteRune(val)
-		case r != 0 && isLetter(val):
-			resBuilder, countBuilder, r = appendRune(resBuilder, countBuilder, r, val)
+		case unicode.IsLetter(runes[i]):
+			switch {
+			case (i+1 < strLen && (runes[i+1] == escapeSymbol || unicode.IsLetter(runes[i+1]))) || i == strLen-1:
+				{
+					resBuilder.WriteRune(runes[i])
+					i++
+				}
+			case i+1 < strLen && unicode.IsDigit(runes[i+1]):
+				{
+					c := runes[i]
+					var count int
+					count, i = getCount(runes, i+1)
+					resBuilder.WriteString(strings.Repeat(string(c), count))
+				}
+			default:
+				return "", 1
+			}
+		case runes[i] == escapeSymbol:
+			{
+				switch {
+				case i+2 < strLen && unicode.IsDigit(runes[i+2]):
+					{
+						c := runes[i+1]
+						var count int
+						count, i = getCount(runes, i+2)
+						resBuilder.WriteString(strings.Repeat(string(c), count))
+					}
+				case i+1 < strLen:
+					{
+						resBuilder.WriteRune(runes[i+1])
+						i += 2
+					}
+				default:
+					return "", 1
+				}
+
+			}
 		default:
 			return "", 1
 		}
-
 	}
-	resBuilder, _, _ = appendRune(resBuilder, countBuilder, r, '1')
 	return resBuilder.String(), 0
 }
 
-func Process2(str string) string {
-	const escapeSymbol = '\\'
-	runes := []rune(str)
-	strLen := utf8.RuneCountInString(str)
-	var resBuilder strings.Builder
-	for i := 0; i < strLen; i++ {
-		end := i + 3
-		if i > strLen {
-			end = strLen
-		}
-		part := runes[i:end]
-		switch {
-		case part[0] != escapeSymbol || len(part) == 1:
-			resBuilder.WriteRune(part[0])
-		case len(part) == 3 && part[0] == escapeSymbol && isNumeric(part[2]):
-			count, _ := strconv.Atoi(string(part[2]))
-			resBuilder.WriteString(strings.Repeat(string(part[1]), count))
-			i = end
-		case part[0] == escapeSymbol:
-			resBuilder.WriteRune(part[1])
-			i = end - 1
-		}
-
-	}
-	return resBuilder.String()
-}
-
-func isLetter(r rune) bool {
-	return 'a' <= r && r <= 'z'
-}
-
-func isNumeric(r rune) bool {
-	return '0' <= r && r <= '9'
-}
-
-func appendRune(resBuilder, countBuilder strings.Builder, r, val rune) (strings.Builder, strings.Builder, rune) {
-	if countBuilder.Len() == 0 {
-		countBuilder.WriteRune('1')
+func getCount(runes []rune, i int) (int, int) {
+	var countBuilder strings.Builder
+	strLen := len(runes)
+	for j := i; j < strLen && unicode.IsDigit(runes[j]); j++ {
+		countBuilder.WriteRune(runes[j])
+		i++
 	}
 	count, _ := strconv.Atoi(countBuilder.String())
-	resBuilder.WriteString(strings.Repeat(string(r), count))
-	r = val
-	countBuilder.Reset()
-	return resBuilder, countBuilder, r
+	return count, i
 }
--- hw3_EDT/process_test.go
+++ hw3_EDT/process_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestProcess1(t *testing.T) {
+func TestProcess(t *testing.T) {
 
 	type result struct {
 		str string
@@ -10,26 +10,16 @@ func TestProcess1(t *testing.T) {
 	}
 
 	testSeq := map[string]result{
-		"a4bc2d5e": {"aaaabccddddde", 0},
-		"abcd":     {"abcd", 0},
-		"a22":      {"aaaaaaaaaaaaaaaaaaaaaa", 0},
-		"45":       {"", 1}}
+		"a4bc2d5e":                {"aaaabccddddde", 0},
+		"abcd":                    {"abcd", 0},
+		"a22":                     {"aaaaaaaaaaaaaaaaaaaaaa", 0},
+		"45":                      {"", 1},
+		"qwe\\45":                 {"qwe44444", 0},
+		"qwe\\4\\5":               {"qwe45", 0},
+		"a4bc2d5eqwe\\\\5j10\\l4": {"aaaabccdddddeqwe\\\\\\\\\\jjjjjjjjjjllll", 0}}
 	for key, val := range testSeq {
-		if str, err := Process1(key); str != val.str || err != val.err {
+		if str, err := Process(key); str != val.str || err != val.err {
 			t.Fatalf("EXPECTED STR %s RESULT STR %s; EXPECTED ERR %d RESULT ERR %d", val.str, str, val.err, err)
-		}
-	}
-}
-
-func TestProcess2(t *testing.T) {
-	testSeq := map[string]string{
-		"qwe\\45":   "qwe44444",
-		"qwe\\4\\5": "qwe45",
-		"qwe\\\\5":  "qwe\\\\\\\\\\",
-	}
-	for key, val := range testSeq {
-		if str := Process2(key); str != val {
-			t.Fatalf("EXPECTED STR %s RESULT STR %s", str, val)
 		}
 	}
 }
