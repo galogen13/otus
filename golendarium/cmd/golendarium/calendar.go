50c6abb1bf1fde186f57b9a5f3220e360b864cfb
Vladislav Elizarov <galogen13@gmail.com>
2020-02-12 07:45:23 +0300

    v2

--- golendarium/cmd/golendarium/calendar.go
+++ golendarium/cmd/golendarium/calendar.go
@@ -1,23 +1,22 @@
-package calendar
+package main
 
 import "time"
 import "github.com/chilts/sid"
 
 type Calendar interface {
-	(cal *calendar) AddEvent(event Event) (event Event, err error)
-	(cal *calendar) DeleteEvent(id string) error
-	(cal *calendar) EditEvent(id string, event Event) error
-	(cal *calendar) GetEvent(id string) (event Event, err error)
-	(cal *calendar) List() []Event
-
+	AddEvent(event Event) error
+	DeleteEvent(id string) error
+	EditEvent(id string, event Event) error
+	GetEvent(id string) (event Event, err error)
+	List() []Event
 }
 
-type Event struct{
-	id string
+type Event struct {
+	id            string
 	Start, Finish time.Time
-	Name, Descr string
+	Name, Descr   string
 }
 
-func NewEvent(start, finish time.Time, name, descr string) Event{
-	return Event{id: sid.Id() , Start:start, Finish: finish, Name:name, Descr: descr}
+func NewEvent(start, finish time.Time, name, descr string) Event {
+	return Event{id: sid.Id(), Start: start, Finish: finish, Name: name, Descr: descr}
 }
--- golendarium/cmd/golendarium/localcalendar.go
+++ golendarium/cmd/golendarium/localcalendar.go
@@ -1,3 +1,26 @@
-package calendar
+package main
+
+type Calendar1 struct {
+	events []Event
+}
+
+func (cal *Calendar1) AddEvent(event Event) error{
+	// проверка на существование
+	cal.events = append(cal.events, event)
+	return
+}
+func (cal *Calendar1) DeleteEvent(id string) error{
+	
+		
+	for i :=0 ; i < len(cal.events); i++{
+		if cal.events[i].id = id{
+			cal.events = append(cal.events[0:i], cal.events[i+1:])
+
+		}
+	}
+}
+	// (cal *Calendar) EditEvent(id string, event Event) error
+	// (cal *Calendar) GetEvent(id string) (event Event, err error)
+	// (cal *Calendar) List() []Event
 
 //var Store []Event
